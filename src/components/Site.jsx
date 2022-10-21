f46456f9282e3da90ec675fe5902c40b314312d9
Justin Barlas <justin_barlas@brown.edu>
2022-10-21 17:02:54 -0400

    update site link

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -4,7 +4,7 @@ export default class Site extends Component {
   render() {
     return (
       <div className='site-container'>
-        <div className='site-button' onClick={() => window.open("https://www.wimp.com", '_blank')}>View the Responsive Redesign!</div></div>
+        <div className='site-button' onClick={() => window.open("https://ominousotter666.github.io/Wimp/", '_blank')}>View the Responsive Redesign!</div></div>
     )
   }
 }
